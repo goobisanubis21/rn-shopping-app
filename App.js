4d47d57022e8b0f541fd60e7ced7b2928307f737
Anthony Parrino <anthony21parrino21@gmail.com>
2021-07-23 22:32:21 -0400

    added redux

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import productsReducer from './store/reducer/product'
+
+const rootReducer = combineReducers({
+  products: productsReducer
+});
+
+const store = createStore(rootReducer)
 
 export default function App() {
   return (
-    <View style={styles.screen}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <Provider store={store}>
+      <View>
+
+      </View>
+    </Provider>
   );
 }
 
---
+++ screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View, FlatList } from 'react-native'
+
+const ProductsOverviewScreen = () => {
+    return (
+        <FlatList />
+    )
+}
+
+export default ProductsOverviewScreen
+
+const styles = StyleSheet.create({})
